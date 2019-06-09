5654955e96eb8c6462fcbba149411dd5395314c1
yossizap <yossizap@gmail.com>
2019-06-09 18:57:03 +0000

    Replaced `at` and `validate` with `set/get_property`
    The previous design expected the user to manually validate tree changes
    and manually modify pointers from the configuration returned by at().
    Now, you can only get a copy of a tree's properties with get_property and
    set_property will automatically go through validation before passing it
    to the actual configuration tree.

--- src/include/jconf/jconf.hpp
+++ src/include/jconf/jconf.hpp
@@ -5,9 +5,11 @@
 #include <string>
 
 #include "nlohmann/json.hpp"
+#include <json-schema.hpp>
 
 namespace jconf {
 
+using json_validator = nlohmann::json_schema::json_validator;
 using json = nlohmann::json;
 
 class Config {
@@ -16,8 +18,8 @@ public:
   Config(std::string storage_path, std::string schema_path);
   void load();
   void save();
-  void validate();
-  json & at(const std::string& key = "");
+  void set_property(const json &property);
+  json get_property(const std::string& key);
   friend std::ostream &operator<<(std::ostream &os, const Config &c);
 
 private:
@@ -25,6 +27,7 @@ private:
   std::string m_schema_path;
   json m_data;
   json m_schema;
+  json_validator m_validator;
 };
 } // namespace jconf
 
